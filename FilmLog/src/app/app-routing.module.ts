3077108542c3e17f058fc2b5697e120d4f9c1f2b
Nyiko Mbhalati <u24722490@tuks.co.za>
2026-04-30 16:33:10 +0200

    Update app routing.

--- FilmLog/src/app/app-routing.module.ts
+++ FilmLog/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './service/auth-guard';
 
 const routes: Routes = [
   // {
@@ -7,18 +8,36 @@ const routes: Routes = [
   //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   // },
   {
-    path: '', 
-    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+    path: '',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule),
   },
   {
     path: 'loader',
-    loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule)
+    loadChildren: () => import('./pages/loader/loader.module').then( m => m.LoaderPageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule),
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'movie-details',
+    loadChildren: () => import('./pages/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'watchlist', loadChildren: () => import('./watchlist/watchlist.module').then(m => m.WatchlistPageModule),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'watched', loadChildren: () => import('./watched/watched.module').then(m => m.WatchedPageModule),
+    canActivate: [AuthGuard]
   }
-
 ];
 @NgModule({
   imports: [
--- FilmLog/src/app/app.module.ts
+++ FilmLog/src/app/app.module.ts
@@ -4,13 +4,27 @@ import { RouteReuseStrategy } from '@angular/router';
 import { FormsModule} from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers } from '@ionic/storage';
+import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver'
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot({
+      name: "FilmLogDB",
+      driverOrder: [ Drivers.IndexedDB, Drivers.LocalStorage]
+    })],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
